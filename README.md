59b2befa592f68cc9c7663ea593d43dc1069b26c
wohangye <ghangprivate@gmail.com>
2018-11-13 01:59:52 +0900

    modified

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## fun script 
 
 just for fun !
-![](https://img.buzzfeed.com/buzzfeed-static/static/2018-08/13/21/campaign_images/buzzfeed-prod-web-02/how-well-do-you-remember-the-fun-song-from-sponge-2-16123-1534210617-0_dblbig.jpg)
+
+<img src = "(https://img.buzzfeed.com/buzzfeed-static/static/2018-08/13/21/campaign_images/buzzfeed-prod-web-02/how-well-do-you-remember-the-fun-song-from-sponge-2-16123-1534210617-0_dblbig.jpg)" width = "300" height = "300" align =center \>
 
 #### A scraping script for baidu wiki
 - search word, give back the content introduce from baidu wiki.
--- scrapBaiduWiki.py
+++ scrapBaiduWiki.py
@@ -6,7 +6,9 @@ import requests
 searchWord = input("input the key word you wanna search:")
 
 session = requests.Session()
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) \
+            AppleWebKit 537.36 (KHTML, like Gecko) Chrome",\
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
 
 
 # searchWord = str(searchWord, "utf-8")
@@ -20,7 +22,7 @@ if html.encoding == 'ISO-8859-1':
     else:
         encoding = html.apparent_encoding
     global encode_content
-    encode_content = html.content.decode(encoding, 'replace') #如果设置为replace，则会用?取代非法字符；
+    encode_content = html.content.decode(encoding, 'replace')
  
 # this html return binary file, but beautifulsoup need text, so return html.text
 # html = urlopen(wikiUrl)
@@ -34,7 +36,7 @@ if contentUrl.encoding == 'ISO-8859-1':
         encoding = encodings[0]
     else:
         encoding = contentUrl.apparent_encoding
-    encode_content = contentUrl.content.decode(encoding, 'replace') #如果设置为replace，则会用?取代非法字符；
+    encode_content = contentUrl.content.decode(encoding, 'replace') 
 
 
 bsObjN = BeautifulSoup(encode_content, "html.parser")
